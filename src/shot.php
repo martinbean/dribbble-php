4fa8deb4295a29d0c5d41bba54db8c6ecb2aca22
Martin Bean <martin@mcbwebdesign.co.uk>
2010-07-29 09:28:04 +0100

    Created shot class

---
+++ src/shot.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Dribbble shot API class
+ *
+ * @author Martin Bean <martin@mcbwebdesign.co.uk>
+ **/
+class Shot extends Dribbble
+{
+    /**
+     * undocumented function
+     *
+     * @param int $page
+     * @param int $per_page
+     * @return string
+     **/
+    public function debuts($page=null, $per_page=null)
+    {
+        $this->paginated_list('/shots/debuts', array($page, $per_page));
+    }
+    
+    /**
+     * undocumented function
+     *
+     * @param int $page
+     * @param int $per_page
+     * @return string
+     **/
+    public function everyone($page=null, $per_page=null)
+    {
+        $this->paginated_list('/shots/everyone', array($page, $per_page));
+    }
+    
+    /**
+     * undocumented function
+     *
+     * @param int $page
+     * @param int $per_page
+     * @return string
+     **/
+    public function popular($page=null, $per_page=null)
+    {
+        $this->paginated_list('/shots/popular', array($page, $per_page));
+    }
+}
