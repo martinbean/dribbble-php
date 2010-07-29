a54ed9a89d5e330a1eb77870ed7d39b4c3f0db22
Martin Bean <martin@mcbwebdesign.co.uk>
2010-07-29 09:31:16 +0100

    Creater player class

---
+++ src/player.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Dribbble player API class
+ *
+ * @author Martin Bean <martin@mcbwebdesign.co.uk>
+ **/
+class Player extends Dribbble
+{
+    /**
+     * Constructs the class
+     *
+     * @param int $id Dribbble player ID
+     * @return void
+     **/
+    function __construct($id=null)
+    {
+        $this->id = $id;
+    }
+    
+    /**
+     * Fetches this player's shots
+     *
+     * @param int $page
+     * @param int $per_page
+     * @return string
+     **/
+    public function shots($page=null, $per_page=null)
+    {
+        $this->paginated_list('/players/'.$this->id.'/shots', array($page, $per_page));
+    }
+    
+    /**
+     * Fetches shots by players that this player follows
+     *
+     * @param int $page
+     * @param int $per_page
+     * @return string
+     **/
+    public function everyone($page=null, $per_page=null)
+    {
+        $this->paginated_list('/players/'.$this->id.'/shots/following', array($page, $per_page));
+    }
+}
