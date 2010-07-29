909183544940d292da5b7af90d9187bdf32f997a
Martin Bean <martin@mcbwebdesign.co.uk>
2010-07-29 09:17:53 +0100

    Created base class

---
+++ src/dribbble.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Dribbble base API class
+ *
+ * @author Martin Bean <martin@mcbwebdesign.co.uk>
+ **/
+class Dribbble
+{
+    /**
+     * Dribbble API base URL
+     *
+     * @var string
+     **/
+    var $baseUrl = "http://api.dribbble.com/";
+}
